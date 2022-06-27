37a71a2e43b03eb7d4ece3032aab0149e64bd7e1
凛 <103912056+irisawa10969@users.noreply.github.com>
2022-06-27 03:05:29 +0000

    kadai1

--- itunes.py
+++ itunes.py
@@ -16,4 +16,4 @@ data = response.json()
 print(json.dumps(data, indent=4, ensure_ascii=False).encode().decode())
 
 # 件数の表示
-print('検索結果：' + str(data['resultCount']) + ' 件')
+print(data['results'][0]['collectionname'])
